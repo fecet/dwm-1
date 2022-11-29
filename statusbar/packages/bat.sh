98ae192bc480af51ec0c776a28f20b409e4e303e
fecet <xiezej@gmail.com>
2022-11-29 23:18:34 +0800

    no bat realize

--- statusbar/packages/bat.sh
+++ statusbar/packages/bat.sh
@@ -12,6 +12,7 @@ update() {
     bat_text=$(acpi -b | sed 2d | awk '{print $4}' | grep -Eo "[0-9]+")
     [ ! "$bat_text" ] && bat_text=$(acpi -b | sed 2d | awk '{print $5}' | grep -Eo "[0-9]+")
     [ ! "$(acpi -b | grep 'Battery 0' | grep Discharging)" ] && charge_icon=""
+    bat_pct=$bat_text%
     if   [ "$bat_text" -ge 95 ]; then bat_icon=""; charge_icon="";
     elif [ "$bat_text" -ge 90 ]; then bat_icon="";
     elif [ "$bat_text" -ge 80 ]; then bat_icon="";
@@ -22,12 +23,12 @@ update() {
     elif [ "$bat_text" -ge 30 ]; then bat_icon="";
     elif [ "$bat_text" -ge 20 ]; then bat_icon="";
     elif [ "$bat_text" -ge 10 ]; then bat_icon="";
-    else bat_icon=""; fi
+    elif [ "$bat_text" -ge 2 ]; then bat_icon="";
+    else bat_icon="" bat_pct=""; fi
 
-    bat_text=$bat_text%
     bat_icon=$charge_icon$bat_icon
 
-    text=" $bat_icon $bat_text "
+    text=" $bat_icon $bat_pct "
     sed -i '/^export '$this'=.*$/d' $DWM/statusbar/temp
     printf "export %s='%s%s%s%s'\n" $this "$color" "$signal" "$text" "$s2d_reset" >> $DWM/statusbar/temp
 }
